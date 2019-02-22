b8fc62d6b6e238dd886d8a64d2a4f02ee8e03de7
wubh2012 <wubh2012@163.com>
2019-02-22 13:18:06 +0800

    add jsdemo.js

---
+++ 5-12/jsdemo.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var dirName = process.argv[2]
+if(fs.existsSync(dirName)){
+    console.log(dirName + ' 目录已经存在')
+}else{
+    fs.mkdirSync("./" + dirName)
+    process.chdir("./" + dirName)
+    fs.mkdirSync('css')
+    fs.mkdirSync('js')
+    
+    fs.writeFileSync('./index.html', "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+    fs.writeFileSync('./css/style.css', "h1{color: red;}")
+    fs.writeFileSync('./js/main.js', 'var string = "Hello World"\nalert(string)')
+    
+    console.log(dirName + ' 目录创建成功')
+}
+
+process.exit(0)
