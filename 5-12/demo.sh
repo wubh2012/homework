a51c920e167120364e8a50a0edc9d2f772b1aa71
wubh2012 <wubh2012@163.com>
2019-02-22 13:25:37 +0800

    完善demo.sh

--- 5-12/demo.sh
+++ 5-12/demo.sh
@@ -7,7 +7,7 @@ else
     mkdir css js
     touch index.html css/style.css js/main.js
     echo -e '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>' >> index.html
-    echo 'h1{color: red;}' >> css/style.css
+    echo -e 'h1{color: red;}' >> css/style.css
     echo -e 'var string = "Hello World"\nalert(string)' >> js/main.js
     echo $1 '创建成功'
     exit
